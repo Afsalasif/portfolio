a1eb1bf207a9539b02cb5ef90c6dbcb6822f77c2
Afsalasif <afsalasif138@gmail.com>
2024-11-30 15:41:21 +0530

    int

---
+++ data/index.ts
@@ -0,0 +1,155 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/bg-1.png",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with every time zones",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "FREELANCER",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Vamoz video sharing platform",
+    des: "a video sharing platform made with next js ",
+    img: "/hye.png",
+    iconLists: [
+      "/re.svg",
+      "/next.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/three.svg",
+      "/fm.svg",
+    ],
+    link: "https://vamoz-final.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "CAB booking site for rp royality",
+    des: "Simplify the cab booking experience by making a tailored website using next js ",
+    img: "/royality.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://rproyality.com/",
+  },
+  {
+    id: 3,
+    title: "HOME GYM",
+    des: "a real workout applicaion made using react js ,featuring home workouts",
+    img: "/GYM.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://homegym.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "GO-GREEN ,waste management app",
+    des: "waste management app made using mern frame work implimenting real user roles",
+    img: "/GREEN.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "sofware engineer at trizent tech pvt ltd",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "sofware engineer at rproyality pvt ltd",
+    desc: "Designed and developed web platform for easy cab booking used react js.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance web developer",
+    desc: "Led the dev of web apps for different clients fom intial satge to deployment",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 4,
+    title: "Lead technical officer at iedc icet",
+    desc: "Developed and maintained the main website for iedc icet",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    href: "https://github.com/Afsalasif",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    href: "https://www.linkedin.com/in/mohamed-afzal-na-731664172/",
+  },
+];
